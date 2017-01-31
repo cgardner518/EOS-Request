6b7358330f448118f54378150269f8ba93af5be5
cgardner <cgardner@zurka.com>
2017-01-31 11:27:03 -0500

    some ironing going on

--- resources/views/orgStructure/index.blade.php
+++ resources/views/orgStructure/index.blade.php
@@ -5,6 +5,15 @@ Organization Structure
 @section('main-content')
 
 <div class="indent-padding width-limited-1200">
+  <div class="form-row org_structure_show_select_div">
+    {!! Form::label('Show: ', '',['class' => 'control-label']) !!}
+    {!! Form::select('structure_format',['Primary Structure', ],'') !!}
+  </div>
+  <div class="pull-right  org_structrue_search_div">
+    <a class="pull-right btn btn-primary btn-gradient" href="/requests/create">New Role</a>
+    <a class="pull-right btn btn-primary btn-gradient" href="/requests/create">New Unit</a><br>
+    <input type="text" name="search" placeholder="Search">
+  </div>
   @each('orgStructure.org-partials.org-structure-partial', $organizations, 'value')
 </div>
 
